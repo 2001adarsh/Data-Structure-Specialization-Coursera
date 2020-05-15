40e6271683fc3acc4e7ccae2e7b3f7a4247d86b7
Adarsh Singh <2001adarshsingh@gmail.com>
2020-05-15 11:58:37 +0530

    Week 1 completed

--- Algorithmic-Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ Algorithmic-Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -1,29 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
 
-int MaxPairwiseProduct(const std::vector<int>& numbers) {
-    int max_product = 0;
-    int n = numbers.size();
-
-    for (int first = 0; first < n; ++first) {
-        for (int second = first + 1; second < n; ++second) {
-            max_product = std::max(max_product,
-                numbers[first] * numbers[second]);
+ll MaxPairwiseProduct(const std::vector<ll>& numbers) {
+    ll max_product = 0;
+    ll n = numbers.size();
+    ll maximum = INT_MIN , secondmaximum = INT_MIN;
+    ll idx = 0;
+    for (ll first = 0; first < n; ++first) {
+            if(maximum < numbers[first]){
+            maximum = max( numbers[first], maximum);
+            idx = first;
+            }
+        }
+    for(ll second = 0; second < n; ++second){
+        if( second != idx){
+            secondmaximum = max(numbers[second], secondmaximum);
         }
     }
-
+  //  cout << maximum << " "<< secondmaximum<<endl;
+    max_product = maximum * secondmaximum;
     return max_product;
 }
 
 int main() {
-    int n;
+
+    ll n;
     std::cin >> n;
-    std::vector<int> numbers(n);
+    std::vector<ll> numbers(n);
     for (int i = 0; i < n; ++i) {
         std::cin >> numbers[i];
     }
-
-    std::cout << MaxPairwiseProduct(numbers); << "\n";
+    std::cout << MaxPairwiseProduct(numbers) << "\n";
     return 0;
+
 }
