b0a04481bc07e1871c79414d80ebee21dbabc62c
Adarsh Singh <2001adarshsingh@gmail.com>
2020-05-17 23:28:47 +0530

    Week 2, 7 questions

--- Algorithmic-Toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
+++ Algorithmic-Toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
 
 int fibonacci_sum_naive(long long n) {
     if (n <= 1)
@@ -18,8 +21,43 @@ int fibonacci_sum_naive(long long n) {
     return sum % 10;
 }
 
+ll fibonacci_sum_fast(ll n) {
+    if(n <= 1)
+        return n;
+    vector<ll> f;
+
+    f.push_back(0);
+    f.push_back(1);
+
+    ll last ;
+    ll secondLast ;
+    ll count = 2;
+    while(true) {
+        last = f[f.size()-1] % 10;
+        secondLast = f[f.size() -2] % 10;
+        f.push_back( (last%10 + secondLast%10)%10 );
+        if(last == 0 && f[f.size()-1] % 10 ==1){
+            count--;
+            break;
+        }
+        count++;
+    }
+    n = n+2;
+    ll res = n % count;
+    res = (f[res] -1) >= 0 ? (f[res] -1) : 9;
+    return ( res );
+}
+
+void test(){
+    for(ll n= 5; n<23; ++n){
+        assert(fibonacci_sum_fast(n) == fibonacci_sum_naive(n));
+    }
+}
+
 int main() {
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_naive(n);
+   // test();
+    cout<<fibonacci_sum_fast(n)<<endl;
+    return 0;
 }
--- Algorithmic-Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
+++ Algorithmic-Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <bits/stdc++.h>
+using namespace std;
 using std::vector;
+typedef long long ll;
 
 long long get_fibonacci_partial_sum_naive(long long from, long long to) {
     long long sum = 0;
@@ -21,8 +24,41 @@ long long get_fibonacci_partial_sum_naive(long long from, long long to) {
     return sum % 10;
 }
 
+ll get_sum_till(ll n) {
+    if(n <= 1)
+        return n;
+    ll count =2;
+    vector<ll> f;
+    f.push_back(0);
+    f.push_back(1);
+    while(true) {
+        ll last = f[f.size() - 1] % 10;
+        ll secondLast = f[f.size() - 2] % 10;
+        f.push_back((last + secondLast)%10);
+        if(last==0 && (f[f.size()-1]%10) ==1) {
+            count--;
+            break;
+        }
+        count++;
+    }
+    return f[n%count];
+}
+
+ll get_fibonacci_partial_sum_fast(ll from, ll to) {
+    ll sum =0;
+    ll x = get_sum_till(to+2);
+    ll y = get_sum_till(from+1);
+   // cout<<x << " "<<y;
+    if(x < y){
+        sum = (x+10) - y;
+    }else{
+        sum = x - y;
+    }
+    return sum;
+}
+
 int main() {
     long long from, to;
     std::cin >> from >> to;
-    std::cout << get_fibonacci_partial_sum_naive(from, to) << '\n';
+    std::cout << get_fibonacci_partial_sum_fast(from, to) << '\n';
 }
